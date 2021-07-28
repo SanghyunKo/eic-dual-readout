d0a2d221cdc2c42848eaecc664d44fe99f9ef09a
chanchoen <75157550+chanchoen@users.noreply.github.com>
2021-07-28 13:41:38 +0900

    Update DRsimDetectorConstruction.cc

--- DRsim/src/DRsimDetectorConstruction.cc
+++ DRsim/src/DRsimDetectorConstruction.cc
@@ -71,7 +71,7 @@ DRsimDetectorConstruction::DRsimDetectorConstruction()
   fDoBarrel = false;
   fDoEndcap = true;
   fDoFiber = true;
-  mTowerMaterial = "Copper"; //"Tungsten"
+  mTowerMaterial = "Copper"; // "Tungsten"
 
   mNumBarrel = 52;
   mNumEndcap = 40;
